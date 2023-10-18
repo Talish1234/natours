647c494be43ec71986d21ce41364fccffdec8f42
Talish1234 <talishtaik1234@gmail.com>
2023-10-19 00:15:24 +0530

    prepared app for deployment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(DB)
   console.log('connected to db');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
